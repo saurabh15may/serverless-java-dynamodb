5db3be3fc03cffceb8efd40e4a4443ac6e2462a2
Saurabh Kumar <skumar@uxcps.local>
2017-09-05 09:28:45 +1000

    Refactor code

--- src/main/java/com/serverless/handler/CreateSuburbHandler.java
+++ src/main/java/com/serverless/handler/CreateSuburbHandler.java
@@ -6,7 +6,7 @@ import com.serverless.model.Suburb;
 import java.util.Map;
 import com.serverless.model.ApiGatewayResponse;
 import com.serverless.util.DynamoDBHelper;
-
+import com.serverless.util.Constants;
 import com.serverless.util.Util;
 import org.apache.log4j.Logger;
 import java.util.HashMap;
@@ -20,21 +20,35 @@ public class CreateSuburbHandler implements RequestHandler<Suburb, ApiGatewayRes
     public ApiGatewayResponse handleRequest(Suburb suburb, Context context) {
         LOG.info("suburb: " + suburb);
 
-        LOG.info("suburbName: " + suburb.getName()+" - postcode: " + suburb.getPostcode());
-
         Util.checkObjectParams(suburb);
 
-        String response = DynamoDBHelper.createSuburbRecord(suburb);
-        
-        Map<String, String> headers = new HashMap<>();
-        headers.put("X-Powered-By", "AWS Lambda & Serverless");
-        headers.put("Content-Type", "application/json");
+        int statusCode = 0;
+        Object responseBody = null;  
+		Map<String, String> headers = createHeader();
+
+        try{
+            //Create record into DynamoDB
+            responseBody = DynamoDBHelper.createSuburbRecord(suburb);
+            statusCode = Constants.STATUS_CODE_SUCCESS;
+        }catch(Exception e){
+            LOG.error("Exception in creating record in DynamoDB "+e);
+            responseBody = Constants.RESPONSE_ERROR;
+            statusCode = Constants.STATUS_CODE_ERROR;
+        }
+
         return ApiGatewayResponse.builder()
-                .setStatusCode(200)
-                .setObjectBody(response)
+                .setStatusCode(statusCode)
+                .setObjectBody(responseBody)
                 .setHeaders(headers)
                 .build();
     }
+
+    private static Map<String, String> createHeader(){
+		Map<String, String> headers = new HashMap<>();
+		headers.put("X-Powered-By", "AWS Lambda & Serverless");
+		headers.put("Content-Type", "application/json");
+		return headers;
+	}
 
 
 }
